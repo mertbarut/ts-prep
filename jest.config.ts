c8b1c280d037761c46753e98baa4236e2c4a21c3
Mert Barut <mebarut@icloud.com>
2023-12-05 22:07:18 +0100

    disabled caching

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,19 @@
-import type { Config } from "@jest/types";
+import type { Config } from '@jest/types'
 export default async (): Promise<Config.InitialOptions> => {
-  return {
-    preset: "ts-jest",
-     displayName: {
-     name: "ts-sandbox",
-     color: "greenBright",
-   },
-   verbose: true,
-   testMatch: ["**/**/*.test.ts"],
-   testEnvironment: "node",
-   detectOpenHandles: true,
-   collectCoverage: true,
-   transform: { "^.+\\.tsx?$": "ts-jest" },
-   globalTeardown: "<rootDir>/src/tests/jest-globals-teardown.ts",
-   forceExit: true,
- };
-};
+    return {
+        preset: 'ts-jest',
+        displayName: {
+            name: 'ts-sandbox',
+            color: 'greenBright',
+        },
+        verbose: true,
+        testMatch: ['**/**/*.test.ts'],
+        testEnvironment: 'node',
+        detectOpenHandles: true,
+        collectCoverage: true,
+        transform: { '^.+\\.tsx?$': 'ts-jest' },
+        globalTeardown: '<rootDir>/src/tests/jest-globals-teardown.ts',
+        forceExit: true,
+        cache: false,
+    }
+}
